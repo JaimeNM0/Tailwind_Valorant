facf5c3d0467390b90da2923c1268d9886d52900
JaimeNM0 <navarro.m.jai@gmail.com>
2023-11-07 17:22:05 +0100

    Sea pulido el código de main.js.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', URL1);
 
-window.onload = function () {
+const modeSwitching = () => {
     let boton = document.getElementById('modo');
 
     boton.addEventListener('click', function () {
@@ -20,30 +20,46 @@ window.onload = function () {
             img_modo.src = './img/luna.png';
         }
     });
+}
+
+const thumbnailInformation = (character) => {
+    let img_completa = document.getElementById('img_completa');
+    let nombre = document.getElementById('nombre');
+    let descripcion = document.getElementById('descripcion');
+    let role = document.getElementById('role');
+    let role_descripcion = document.getElementById('role_descripcion');
+    let habilidades = [
+        { name: document.getElementById('habilidad1'), displayIcon: document.getElementById('img_habilidad1') },
+        { name: document.getElementById('habilidad2'), displayIcon: document.getElementById('img_habilidad2') },
+        { name: document.getElementById('habilidad3'), displayIcon: document.getElementById('img_habilidad3') },
+        { name: document.getElementById('habilidad4'), displayIcon: document.getElementById('img_habilidad4') }
+    ];
+
+    img_completa.innerHTML = '';
+    img_completa.style.width = '550px';
+    img_completa.src = character.fullPortrait;
+
+    nombre.innerHTML = character.displayName;
+    descripcion.innerHTML = character.description;
+    role.innerHTML = character.role.displayName;
+    role_descripcion.innerHTML = character.role.description;
+
+    i = 0;
+    character.abilities.forEach(characterAbilities => {
+        habilidades[i].name.innerHTML = characterAbilities.displayName;
+        habilidades[i].displayIcon.src = characterAbilities.displayIcon;
+        ++i;
+    });
+}
+
+window.onload = function () {
+
+    modeSwitching();
 
     xhr.onreadystatechange = (ev) => {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.response);
-            console.log(xhr.response.data);
-            // 1.
             let informacion = xhr.response.data;
-            console.log(informacion);
 
-            let unNombre = informacion[2].displayName;
-            console.log(unNombre);
-            // 2. Forma de Alejandro:
-            const nombres = informacion.map((informacion) => informacion.displayName);
-            // 2. Mi forma:
-            let listaNombres = [];
-            informacion.forEach(element => {
-                listaNombres.push(element.displayName);
-            });
-            console.log('listaNombres: ' + listaNombres);
-            // 2. Mi forma mejorada:
-            //TODO No funciona.
-            /*let listaNombre = informacion.forEach(element => { listaNombres.push(element.displayName) });
-            console.log('listaNombre: ' + listaNombre);*/
-            // 3.
             let listaImagenes = [];
             informacion.forEach(element => {
                 if (element.displayIcon != null && element.fullPortrait != null) {
@@ -55,43 +71,7 @@ window.onload = function () {
                     miniatura.style.border = '1px solid #888';
 
                     miniatura.addEventListener('click', function () {
-                        let div_imagen_completa = document.getElementById('imagen_completa');
-                        let nombre = document.getElementById('nombre');
-                        let descripcion = document.getElementById('descripcion');
-                        let role = document.getElementById('role');
-                        let role_descripcion = document.getElementById('role_descripcion');
-                        let habilidad1 = document.getElementById('habilidad1');
-                        let habilidad2 = document.getElementById('habilidad2');
-                        let habilidad3 = document.getElementById('habilidad3');
-                        let habilidad4 = document.getElementById('habilidad4');
-                        let img_habilidad1 = document.getElementById('img_habilidad1');
-                        let img_habilidad2 = document.getElementById('img_habilidad2');
-                        let img_habilidad3 = document.getElementById('img_habilidad3');
-                        let img_habilidad4 = document.getElementById('img_habilidad4');
-                        let habilidades = [
-                            { name: habilidad1, displayIcon: img_habilidad1 },
-                            { name: habilidad2, displayIcon: img_habilidad2 },
-                            { name: habilidad3, displayIcon: img_habilidad3 },
-                            { name: habilidad4, displayIcon: img_habilidad4 }
-                        ];
-
-                        div_imagen_completa.innerHTML = '';
-                        const completa = document.createElement('img');
-                        completa.style.width = '550px';
-                        completa.src = element.fullPortrait;
-                        div_imagen_completa.appendChild(completa);
-
-                        nombre.innerHTML = element.displayName;
-                        descripcion.innerHTML = element.description;
-                        role.innerHTML = element.role.displayName;
-                        role_descripcion.innerHTML = element.role.description;
-
-                        i = 0;
-                        element.abilities.forEach(element_abilities => {
-                            habilidades[i].name.innerHTML = element_abilities.displayName;
-                            habilidades[i].displayIcon.src = element_abilities.displayIcon;
-                            ++i;
-                        });
+                        thumbnailInformation(element);
                     });
 
                     miniatura.src = element.displayIcon;
@@ -101,7 +81,6 @@ window.onload = function () {
             console.log(listaImagenes);
         }
     }
-
 
     xhr.send();
     xhr.responseType = 'json';
