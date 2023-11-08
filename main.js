96c19c7fe99bc947c01517a19bbcd844e1b7c146
JaimeNM0 <navarro.m.jai@gmail.com>
2023-11-08 17:45:04 +0100

    Se ha modificado index.html para tener los divs de weapons, skins y video y main.js para rellenarlos.

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-const URL1 = 'https://valorant-api.com/v1/agents';
-
-const xhr = new XMLHttpRequest();
-
-xhr.open('GET', URL1);
+const AGENTS_URL = 'https://valorant-api.com/v1/agents';
+const WEAPONS_URL = 'https://valorant-api.com/v1/weapons';
 
 const modeSwitching = () => {
     let buttonMode = document.getElementById('mode');
@@ -20,6 +17,41 @@ const modeSwitching = () => {
             imgMode.src = './img/luna.png';
         }
     });
+}
+
+const loadAgents = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', AGENTS_URL);
+    xhr.responseType = 'json';
+
+    xhr.onreadystatechange = (ev) => {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            let dataCharacters = xhr.response.data;
+
+            //let listaImagenes = [];
+            dataCharacters.forEach(element => {
+                if (element.displayIcon != null && element.fullPortrait != null) {
+                    //listaImagenes.push({ name: element.displayName, icon: element.displayIcon, full: element.fullPortrait });
+                    let divCharacterMiniatures = document.getElementById('characterMiniatures');
+
+                    const miniature = document.createElement('img');
+                    miniature.style.width = '80px';
+                    miniature.style.border = '1px solid #888';
+
+                    miniature.addEventListener('click', function () {
+                        characterThumbnailInformation(element);
+                        loadWeapons();
+                    });
+
+                    miniature.src = element.displayIcon;
+                    divCharacterMiniatures.appendChild(miniature);
+                }
+            });
+            //console.log(listaImagenes);
+        }
+    }
+
+    xhr.send();
 }
 
 const characterThumbnailInformation = (character) => {
@@ -52,36 +84,85 @@ const characterThumbnailInformation = (character) => {
     });
 }
 
-window.onload = function () {
+const loadWeapons = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', WEAPONS_URL);
+    xhr.responseType = 'json';
 
-    modeSwitching();
-
-    xhr.onreadystatechange = (ev) => {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            let dataCharacters = xhr.response.data;
-
-            //let listaImagenes = [];
-            dataCharacters.forEach(element => {
-                if (element.displayIcon != null && element.fullPortrait != null) {
-                    //listaImagenes.push({ name: element.displayName, icon: element.displayIcon, full: element.fullPortrait });
-                    let divCharacterMiniatures = document.getElementById('characterMiniatures');
-
-                    const miniature = document.createElement('img');
-                    miniature.style.width = '80px';
-                    miniature.style.border = '1px solid #888';
-
-                    miniature.addEventListener('click', function () {
-                        characterThumbnailInformation(element);
-                    });
-
-                    miniature.src = element.displayIcon;
-                    divCharacterMiniatures.appendChild(miniature);
-                }
-            });
-            //console.log(listaImagenes);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.response.data);
+            let weapons = xhr.response.data;
+            generaWeapons(weapons);
         }
     }
 
     xhr.send();
+}
+
+const generaWeapons = (weapons) => {
+    let miniatureWeapons = document.getElementById('miniatureWeapons');
+    miniatureWeapons.innerHTML = '';
+
+    weapons.forEach(element => {
+        const arma = document.createElement('img');
+        arma.style.width = '80px';
+        arma.src = element.displayIcon;
+
+        arma.addEventListener('click', function () {
+            console.log(element.uuid);
+            let miniatureSkins = document.getElementById('miniatureSkins');
+            miniatureSkins.innerHTML = '';
+            element.skins.forEach((skin) => {
+                loadAndGenerateWeaponsSkinsData(skin, miniatureSkins);
+            });
+        });
+
+        miniatureWeapons.appendChild(arma);
+    });
+
+}
+
+const loadAndGenerateWeaponsSkinsData = (skin, miniatureSkins) => {
+    //https://valorant-api.com/v1/weapons/skinlevels/uuid
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', WEAPONS_URL + '/skinlevels/' + skin.levels[0].uuid);
     xhr.responseType = 'json';
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && skin.displayIcon !== null) {
+            const skinDetail = xhr.response.data;
+            //console.log(skinDetail);
+
+
+            const imgSkin = document.createElement('img');
+            imgSkin.style.width = '100px';
+            imgSkin.style.height = '80px';
+            imgSkin.src = skin.displayIcon;
+
+            imgSkin.addEventListener('click', function () {
+                console.log(skinDetail.streamedVideo);
+                let divVideo = document.getElementById('video');
+                divVideo.innerHTML = '';
+
+                if (skinDetail.streamedVideo !== null) {
+                    const iframe = document.createElement('iframe');
+                    iframe.src = skinDetail.streamedVideo;
+                    iframe.style.width = '1000px';
+                    iframe.style.height = '570px';
+                    divVideo.appendChild(iframe);
+                }
+            });
+
+            miniatureSkins.appendChild(imgSkin);
+        }
+    }
+
+    xhr.send();
+}
+
+
+window.onload = function () {
+    modeSwitching();
+    loadAgents();
 }
