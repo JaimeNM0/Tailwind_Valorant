21301654ab0523563d4142a7b388d2a9f17f0102
JaimeNM0 <navarro.m.jai@gmail.com>
2023-11-08 16:44:51 +0100

    Se ha puesto todas las variables en ingles y sea pulido el código.

--- main.js
+++ main.js
@@ -5,49 +5,49 @@ const xhr = new XMLHttpRequest();
 xhr.open('GET', URL1);
 
 const modeSwitching = () => {
-    let boton = document.getElementById('modo');
+    let buttonMode = document.getElementById('mode');
 
-    boton.addEventListener('click', function () {
+    buttonMode.addEventListener('click', function () {
         let html = document.getElementById('html');
-        let img_modo = document.getElementById('img_modo');
+        let imgMode = document.getElementById('imgMode');
         if (html.className === 'dark') {
             html.className = 'light';
             document.documentElement.classList.remove('dark');
-            img_modo.src = './img/sol.png';
+            imgMode.src = './img/sol.png';
         } else {
             html.className = 'dark';
             document.documentElement.classList.add('dark');
-            img_modo.src = './img/luna.png';
+            imgMode.src = './img/luna.png';
         }
     });
 }
 
-const thumbnailInformation = (character) => {
-    let img_completa = document.getElementById('img_completa');
-    let nombre = document.getElementById('nombre');
-    let descripcion = document.getElementById('descripcion');
-    let role = document.getElementById('role');
-    let role_descripcion = document.getElementById('role_descripcion');
-    let habilidades = [
-        { name: document.getElementById('habilidad1'), displayIcon: document.getElementById('img_habilidad1') },
-        { name: document.getElementById('habilidad2'), displayIcon: document.getElementById('img_habilidad2') },
+const characterThumbnailInformation = (character) => {
+    let imgFullPictureCharacter = document.getElementById('imgFullPictureCharacter');
+    let nameCharacter = document.getElementById('nameCharacter');
+    let descriptionCharacter = document.getElementById('descriptionCharacter');
+    let roleCharacter = document.getElementById('roleCharacter');
+    let roleDescriptionCharacter = document.getElementById('roleDescriptionCharacter');
+    let abilities = [
+        { name: document.getElementById('abilityCharacter1'), displayIcon: document.getElementById('imgAbilityCharacter1') },
+        { name: document.getElementById('abilityCharacter2'), displayIcon: document.getElementById('img_habilidad2') },
         { name: document.getElementById('habilidad3'), displayIcon: document.getElementById('img_habilidad3') },
         { name: document.getElementById('habilidad4'), displayIcon: document.getElementById('img_habilidad4') }
     ];
 
-    img_completa.innerHTML = '';
-    img_completa.style.width = '550px';
-    img_completa.src = character.fullPortrait;
+    imgFullPictureCharacter.innerHTML = '';
+    imgFullPictureCharacter.style.width = '550px';
+    imgFullPictureCharacter.src = character.fullPortrait;
 
-    nombre.innerHTML = character.displayName;
-    descripcion.innerHTML = character.description;
-    role.innerHTML = character.role.displayName;
-    role_descripcion.innerHTML = character.role.description;
+    nameCharacter.innerHTML = character.displayName;
+    descriptionCharacter.innerHTML = character.description;
+    roleCharacter.innerHTML = character.role.displayName;
+    roleDescriptionCharacter.innerHTML = character.role.description;
 
     i = 0;
     character.abilities.forEach(characterAbilities => {
-        habilidades[i].name.innerHTML = characterAbilities.displayName;
-        habilidades[i].displayIcon.src = characterAbilities.displayIcon;
+        abilities[i].name.innerHTML = characterAbilities.displayName;
+        abilities[i].displayIcon.src = characterAbilities.displayIcon;
         ++i;
     });
 }
@@ -58,27 +58,27 @@ window.onload = function () {
 
     xhr.onreadystatechange = (ev) => {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            let informacion = xhr.response.data;
+            let dataCharacters = xhr.response.data;
 
-            let listaImagenes = [];
-            informacion.forEach(element => {
+            //let listaImagenes = [];
+            dataCharacters.forEach(element => {
                 if (element.displayIcon != null && element.fullPortrait != null) {
-                    listaImagenes.push({ name: element.displayName, icon: element.displayIcon, full: element.fullPortrait });
-                    let div_miniaturas = document.getElementById('miniaturas');
+                    //listaImagenes.push({ name: element.displayName, icon: element.displayIcon, full: element.fullPortrait });
+                    let divCharacterMiniatures = document.getElementById('characterMiniatures');
 
-                    const miniatura = document.createElement('img');
-                    miniatura.style.width = '80px';
-                    miniatura.style.border = '1px solid #888';
+                    const miniature = document.createElement('img');
+                    miniature.style.width = '80px';
+                    miniature.style.border = '1px solid #888';
 
-                    miniatura.addEventListener('click', function () {
-                        thumbnailInformation(element);
+                    miniature.addEventListener('click', function () {
+                        characterThumbnailInformation(element);
                     });
 
-                    miniatura.src = element.displayIcon;
-                    div_miniaturas.appendChild(miniatura);
+                    miniature.src = element.displayIcon;
+                    divCharacterMiniatures.appendChild(miniature);
                 }
             });
-            console.log(listaImagenes);
+            //console.log(listaImagenes);
         }
     }
 
